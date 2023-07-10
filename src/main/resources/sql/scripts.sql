f30cb57701f537822a2cc3b10c4df84cad6d6d66
anca.grigoras <anca.grigoras88@gmail.com>
2023-07-10 11:49:20 +0200

    Add sql scripts

---
+++ src/main/resources/sql/scripts.sql
@@ -0,0 +1,21 @@
+create database bank;
+
+use bank;
+
+create table users(
+    id int not null auto_increment,
+    username varchar(45) not null,
+    password varchar(45) not null,
+    enabled int not null,
+    primary key (id)
+);
+
+create table authorities(
+    id int not null auto_increment,
+    username varchar(45) not null,
+    password varchar(45) not null,
+    primary key (id)
+);
+
+insert ignore into users values (null, 'happy', '12345', '1');
+insert ignore into authorities values (null, 'happy', 'write');
